40a257901cda3469a5786b603dc2d1e53cbfb9f8
Abhith Rajan <dearabhith@gmail.com>
2018-05-24 23:56:12 +0400

    routes renamed

--- src/navigation/root-navigator.ts
+++ src/navigation/root-navigator.ts
@@ -1,18 +1,18 @@
 import { StackNavigator } from "react-navigation"
-import { Splash } from "../views/piggy/splash/splash-screen"
+// import { Splash } from "../views/piggy/splash/splash-screen"
 import { Home } from "../views/piggy/home/home-screen"
 import { Login } from "../views/account/login/login-screen"
 // import { ExampleNavigator } from "./example-navigator"
 
 export const RootNavigator = StackNavigator(
   {
-    Splash: { screen: Splash },
-    HomeNavigator: { screen: Home },
-    LoginNavigator: { screen: Login },
+    // Splash: { screen: Splash },
+    Home: { screen: Home },
+    Login: { screen: Login },
     // exampleStack: { screen: ExampleNavigator },
   },
   {
-    initialRouteName: "Splash",
+    initialRouteName: "Home",
     headerMode: "none",
     navigationOptions: { gesturesEnabled: false },
   },
