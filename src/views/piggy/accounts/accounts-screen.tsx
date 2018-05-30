fd1ca8468d8053603b539d8d9dc9156195f3d4bc
Abhith Rajan <dearabhith@gmail.com>
2018-05-30 23:26:36 +0400

    accounts screen initial

--- src/views/piggy/accounts/accounts-screen.tsx
+++ src/views/piggy/accounts/accounts-screen.tsx
@@ -1,19 +1,29 @@
 import * as React from "react"
 import { observer } from "mobx-react"
-import { Text } from "../../shared/text"
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
+import { Header } from "react-native-elements"
+import { AccountStore } from "../../../models/account-store/account-store"
+import { AccountList } from "../../shared/account-list"
 
 export interface AccountsScreenProps extends NavigationScreenProps<{}> {
+  accountStore: AccountStore
 }
 
-// @inject("mobxstuff")
 @observer
 export class Accounts extends React.Component<AccountsScreenProps, {}> {
-  render () {
+  componentDidMount() {
+    this.refresh()
+  }
+  refresh(){
+    this.props.accountStore.getTenantAccountsAsync()
+  }
+  render() {
     return (
-      <Screen preset="fixedCenter">
-        <Text preset="header" tx="Accounts.header" />
+      <Screen preset="scrollStack">
+        <Header centerComponent={{ text: "Accounts", style: { color: "#fff" } }} />
+        <AccountList title="Accounts" accounts={this.props.accountStore.userAccounts} isLoading={this.props.accountStore.isLoading} onRefresh={this.refresh}/>
+        <AccountList title="Family Accounts" accounts={this.props.accountStore.userAccounts} isLoading={this.props.accountStore.isLoading} onRefresh={this.refresh}/>
       </Screen>
     )
   }
