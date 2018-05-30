2013317765d4b52d7534530db1bd4d76efdf3a09
Abhith Rajan <dearabhith@gmail.com>
2018-05-30 23:22:12 +0400

    account store model

---
+++ src/models/account-store/account-store.ts
@@ -0,0 +1,45 @@
+import { types, flow, getEnv } from "mobx-state-tree"
+import { Environment } from "../environment"
+import { AccountModel } from "../account/account"
+
+export const AccountStoreModel = types
+  .model("AccountStore")
+  .props({
+    userAccounts: types.optional(types.array(AccountModel), []),
+    othersAccounts: types.optional(types.array(AccountModel), []),
+    status: types.optional(types.enumeration(["idle", "pending", "done", "error"]), "idle"),
+  }) // Setters
+  .actions(self => ({
+    setStatus(value?: "idle" | "pending" | "done" | "error") {
+      self.status = value
+    },
+  }))
+  .views(self => ({
+    get environment() {
+      return getEnv(self) as Environment
+    },
+    get isLoading() {
+      return self.status === "pending"
+    },
+  }))
+  .actions(self => ({
+    getTenantAccountsAsync: flow(function*() {
+      self.setStatus("pending")
+      try {
+        const result = yield self.environment.api.getTenantAccounts()
+        if (result.kind === "ok") {
+          self.userAccounts = result.userAccounts
+          self.othersAccounts = result.otherMembersAccounts
+          self.setStatus("done")
+        } else {
+          self.setStatus("error")
+        }
+      } catch (error) {
+        self.setStatus("error")
+      }
+    }),
+  }))
+  .views(self => ({}))
+
+type AccountStoreType = typeof AccountStoreModel.Type
+export interface AccountStore extends AccountStoreType {}
