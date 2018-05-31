dd8705b167206d04a872984872ec0676d2fef08f
Abhith Rajan <dearabhith@gmail.com>
2018-05-31 22:51:41 +0400

    section list introduced

--- src/views/piggy/accounts/accounts-screen.tsx
+++ src/views/piggy/accounts/accounts-screen.tsx
@@ -2,28 +2,45 @@ import * as React from "react"
 import { observer } from "mobx-react"
 import { NavigationScreenProps } from "react-navigation"
 import { Screen } from "../../shared/screen"
-import { Header } from "react-native-elements"
+import { Header, ListItem } from "react-native-elements"
 import { AccountStore } from "../../../models/account-store/account-store"
-import { AccountList } from "../../shared/account-list"
+import { SectionList } from "react-native"
+import { AccountCell } from "../account-cell"
+import { ViewStyle } from "react-native";
+import { spacing } from "../../../theme";
 
 export interface AccountsScreenProps extends NavigationScreenProps<{}> {
   accountStore: AccountStore
 }
+
+const ACCOUNT_HEADER: ViewStyle = { marginTop: spacing[2] }
 
 @observer
 export class Accounts extends React.Component<AccountsScreenProps, {}> {
   componentDidMount() {
     this.refresh()
   }
-  refresh(){
+  refresh() {
     this.props.accountStore.getTenantAccountsAsync()
+  }
+  renderRow = ({ item }) => {
+    return <AccountCell account={item} />
+  }
+  renderSectionHeader = headerItem => {
+   return <ListItem bottomDivider containerStyle={ACCOUNT_HEADER} title={headerItem.section.title} />
   }
   render() {
     return (
       <Screen preset="scrollStack">
         <Header centerComponent={{ text: "Accounts", style: { color: "#fff" } }} />
-        <AccountList title="My Accounts" accounts={this.props.accountStore.userAccounts} isLoading={this.props.accountStore.isLoading} onRefresh={this.refresh}/>
-        <AccountList title="Family Accounts" accounts={this.props.accountStore.userAccounts} isLoading={this.props.accountStore.isLoading} onRefresh={this.refresh}/>
+        <SectionList
+          renderItem={this.renderRow}
+          renderSectionHeader={this.renderSectionHeader}
+          sections={this.props.accountStore.accounts}
+          onRefresh={() => this.refresh()}
+          refreshing={this.props.accountStore.isLoading}
+          keyExtractor={item => item.id}
+        />
       </Screen>
     )
   }
