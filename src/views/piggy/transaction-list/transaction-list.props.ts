872a5cf4d4ea8e0f6075a8f4e4a566108cbe1583
Abhith Rajan <dearabhith@gmail.com>
2018-06-05 22:56:00 +0400

    transaction list component

---
+++ src/views/piggy/transaction-list/transaction-list.props.ts
@@ -0,0 +1,28 @@
+import { ViewStyle, TouchableOpacityProperties } from "react-native"
+import { TransactionListPresetNames } from "./transaction-list.presets"
+
+export interface TransactionListProps extends TouchableOpacityProperties {
+  /**
+   * Text which is looked up via i18n.
+   */
+  tx?: string
+
+  /**
+   * The text to display if not using `tx` or nested components.
+   */
+  text?: string
+
+  /**
+   * An optional style override useful for padding & margin.
+   */
+  style?: ViewStyle
+
+  /**
+   * One of the different types of text presets.
+   */
+  preset?: TransactionListPresetNames
+
+  onRefresh(): void
+  groupedTransactions: any
+  loading: boolean
+}
